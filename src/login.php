2cc7d7326e4e69bbb1677fcc04d2b4a678e210ca
Denver Novencido <dnovencido@gmail.com>
2022-12-05 11:38:14 +0800

    Added login page

--- src/login.php
+++ src/login.php
@@ -1,14 +1,10 @@
 <?php
-    include "functions.php"; 
+    include "functions.php";
     include "session.php";
 
     $errors = [];
 
     if($_POST['submit']) {
-
-        if(!$_POST['name']){
-            $errors[] = "Name is required";
-        }
 
         if(!$_POST['email']){
             $errors[] = "Email is required";
@@ -19,40 +15,31 @@
         }
 
         if(empty($errors)) {
-            if(!check_existing_email($_POST['email'])) {
-                $user = save_registration($_POST['name'], $_POST['email'], $_POST['password']);
-                if(!empty($user)) {
-                    $_SESSION['id'] = $user['id'];
-                    $_SESSION['name'] = $user['name'];
-    
-                    header("Location: account.php");
-                } else {
-                    $errors[] = "There was an error logging in your account.";
-                }
+            $user = login_account($_POST['email'], $_POST['password']);
+            if(!empty($user)) {
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['name'] = $user['name'];
+
+                header("Location: account.php");
             } else {
-                $errors[] = "The email address is already existing.";
+                $errors[] = "The email address or password that you've entered does not match any account.";
             }
         }
-        
     }
 ?>
 <?php include "layouts/_header.php"; ?>
             <header>
-                <?php include "layouts/_navigation.php"; ?>
+                <?php include("layouts/_navigation.php"); ?>
             </header>
             <section>
                 <div class="container">
-                    <div id="register">
-                        <div id="register-form">
-                            <h1>Register an account.</h1>
+                    <div id="login">
+                        <div id="login-form">
+                            <h1>Login your account.</h1>
                             <?php if(!empty($errors)) { ?>
                                 <?php include "layouts/_error-messages.php" ?>
-                            <?php } ?> 
+                            <?php } ?>
                             <form method="post">
-                                <div class="input-control">
-                                    <label for="name">Name: </label>
-                                    <input type="text" name="name" class="input-field" value="<?= $_POST['name'] ?>" />
-                                </div>
                                 <div class="input-control">
                                     <label for="name">Email: </label>
                                     <input type="email" name="email" class="input-field" value="<?= $_POST['email'] ?>" />
@@ -62,11 +49,11 @@
                                     <input type="password" name="password" class="input-field" value="<?= $_POST['password'] ?>" />
                                 </div>
                                 <div class="input-control">
-                                    <input type="submit" name="submit" class="btn btn-md btn-rounded" value="Register" />
+                                    <input type="submit" name="submit" class="btn btn-md btn-rounded" value="Login" />
                                 </div>
                             </form>
                         </div>                
                     </div>
-                </div>                
+                </div>
             </section>
 <?php include "layouts/_footer.php"; ?>
